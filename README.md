082c9d72c31ffce860f16e27dd9ca7f1e73690c4
khoamai291299 <khoamai2912@gmail.com>
2025-12-02 09:29:21 +0700

    Add file Inventory.py and Update file Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # inventory-manager-24S1020031
 Bài kiểm tra số 1
+# Họ và tên: Mai Đăng Khoa
+# Mã sinh viên: 24S1020031
---
+++ inventory.py
@@ -0,0 +1,12 @@
+# 24S1020031 - Mai Đăng Khoa
+products = []
+
+def main():
+    print('Menu quản lý Inventory:')
+    print('_______________________')
+    print('1. Thêm sản phẩm')
+    print('2. Xem danh sách sản phẩm')
+    print('3. Duyệt danh sách sản phẩm có số lượng dưới 5')
+    print('4. Thoát chương trình!!!')
+    print('_______________________')
+
